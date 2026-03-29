6a50a53676222ce70414a47e776d6b814e106771
SHRADDHA <cshraddha621@gmail.com>
2026-03-29 08:23:16 +0530

    Fixed build errors: Added missing using System.Linq; directives to controllers

--- eportal-api/obj/Debug/net8.0/EPortalApi.AssemblyInfo.cs
+++ eportal-api/obj/Debug/net8.0/EPortalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPortalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f156b3538193d10e0e02b7667fad274f4ab59070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8952813b6e6f0334805fbef152b5fcbc99a3ef05")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPortalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPortalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
