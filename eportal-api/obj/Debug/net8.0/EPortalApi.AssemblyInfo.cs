04b25dacec16eb94e9d5474596e63f718d05a34f
SHRADDHA <cshraddha621@gmail.com>
2026-04-28 01:59:35 +0530

    chore: commit remaining build artifacts and env template

--- eportal-api/obj/Debug/net8.0/EPortalApi.AssemblyInfo.cs
+++ eportal-api/obj/Debug/net8.0/EPortalApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPortalApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a50a53676222ce70414a47e776d6b814e106771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7355a0bb181662a3d41d624f1d10354659d67a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPortalApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPortalApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
